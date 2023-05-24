4ac814a3f7364543fe7c006623ce5d11b8b2c68b
elinkare <127119787+elinkare@users.noreply.github.com>
2023-05-24 11:17:32 +0200

    Code authentication (#24)
    * work in progress
    
    * work in progress
    
    * work in progress
    
    * work in progress

--- monitor/monitor.c
+++ monitor/monitor.c
@@ -5,12 +5,20 @@
 #include "payload.h"
 #include "ring_buffer.h"
 #include "s3k.h"
+#include "code-auth.h"
+#include "aes128.h"
 
 #include <stdbool.h>
 #include <string.h>
 
 #define MONITOR_PID 0
 #define APP_PID 1
+
+/*Allocate memory for app: */
+uint8_t app_mem[MEMSIZE] NOINIT ALIGNED(MEMSIZE);
+
+// Hypothetically provided at distribution of code/application.
+uint8_t signature[16] = {0x39, 0x25, 0x84, 0x1d, 0x02, 0xdc, 0x09, 0xfb, 0xdc, 0x11, 0x85, 0x97, 0x19, 0x6a, 0x0b, 0x32};
 
 uint8_t pmpcaps[8] = { 0 };
 
@@ -40,14 +48,6 @@ void setup_monitor(void)
 
 void setup_app(void) 
 {
-	/* Give monitor access to APP memory */
-	capman_derive_pmp(0x20, APP_BASE, APP_BASE + 0x10000, S3K_RWX);
-	pmpcaps[1] = 0x20;
-	capman_setpmp(pmpcaps);
-
-	/* Copy over binary */
-	memcpy((void *)APP_BASE, app_bin, app_bin_len);
-
 	/* Give APP PMP slice */
 	capman_mgivecap(APP_PID, 0x20, 0x0);
 	/* Give APP memory slice */
@@ -65,8 +65,23 @@ void setup_app(void)
 	capman_msetreg(APP_PID, S3K_REG_PMP, 0x0100);
 }
 
+void load_app()
+{
+	alt_puts("inside load_app");
+	
+	/* Give monitor access to APP memory */
+	capman_derive_pmp(0x20, APP_BASE, APP_BASE + 0x10000, S3K_RWX);
+	pmpcaps[1] = 0x20;
+	capman_setpmp(pmpcaps);
+
+	/*Copy app-binary to allocated memory: */
+	/*alt_printf("%d", app_bin_len); 6162*/
+	memcpy((void *)APP_BASE, app_bin, app_bin_len);
+}
+
 void setup(void)
 {
+	alt_puts("inside setup");
 	/* Delete time on core 2 3 4. */
 	s3k_delcap(4);
 	s3k_delcap(5);
@@ -81,6 +96,12 @@ void setup(void)
 	/* Setup memory and time */
 	setup_memory_slices();
 	setup_time_slices();
+	
+	/* Load app to allocated memory */
+	load_app();
+
+	/* Verify app with CBC-MAC*/
+	code_auth((void *)APP_BASE, signature, app_bin_len);
 
 	/* Setup app */
 	setup_app();
